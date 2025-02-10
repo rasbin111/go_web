8045910ae4491b280f56227cdcf51cc89a98eedf
rasbin <rasbin.thapa@learnsic.com>
2025-02-10 18:46:27 +0545

    handling post request, reading path and query parameters added

--- server.go
+++ server.go
@@ -6,11 +6,46 @@ import (
     "github.com/labstack/echo/v4"
 )
 
+func getUser(c echo.Context) error {
+    id := c.Param("id")
+    return c.String(http.StatusOK, id)
+}
+
+func show(c echo.Context) error {
+    team := c.QueryParam("team")
+    member := c.QueryParam("member")
+    return c.String(http.StatusOK, "team: " + team + ", member: " + member)
+}
+
+func save(c echo.Context) error {
+    name := c.FormValue("name")
+    email := c.FormValue("email")
+
+    return c.String(http.StatusOK, "name:" + name + ", email:" + email)
+}
+
 func main(){
     e := echo.New()
+
+
+    // path parameters 
+    e.GET("/users/:id", getUser)
+
+    // query parameters
+    e.GET("/show", show)
+
+    // post
+    e.POST("/save", save)
+
     e.GET("/", func (c echo.Context) error {
         return c.String(http.StatusOK, "Hello world")
     })
 
     e.Logger.Fatal(e.Start(":1323"))
+
+
 }
+
+
+
+
